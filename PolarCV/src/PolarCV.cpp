884bdc67003de4b586cb1ca3107de27e6c191bd2
aaronkarp123 <aaronkarp123@gmail.com>
2022-11-27 11:16:53 -0500

    Updated plugin

--- PolarCV/src/PolarCV.cpp
+++ PolarCV/src/PolarCV.cpp
@@ -17,6 +17,7 @@ struct PolarCV : Module {
         SPEED_DIAL_PARAM,
         A_DIAL_PARAM,
         B_DIAL_PARAM,
+        CUR_EQ_PARAM,
         PARAMS_LEN
     };
     enum InputId {
@@ -30,7 +31,7 @@ struct PolarCV : Module {
         OUTPUTS_LEN
     };
     enum LightId {
-        SPEED_MULT_LIGHT,
+        ENUMS(SPEED_MULT_LIGHT, 3),
         PREV_EQ_LIGHT,
         NEXT_EQ_LIGHT,
         LIGHTS_LEN
@@ -44,7 +45,11 @@ struct PolarCV : Module {
     bool prev_prev_eq_trig = false;
     int time_mult_idx = 0;
     float time_mults[3] = {0.5f, 1.f, 2.f};
+    int color_lights[3][3] = {{255, 0, 255}, {0, 255, 255}, {255, 255, 0}};
     bool prev_time_mult = false;
+    bool speed_input_connected = false;
+    float speed = 0.f;
+    float speed_range_slope = 1.0 * (0.02f - 0.0001f) / (0.f - 10.f);
     
     struct Point {
         float minX[16] = {};
@@ -62,19 +67,17 @@ struct PolarCV : Module {
         }
     };
     
-    Point pointBuffer[BUFFER_SIZE];
-    Point currentPoint;
-    
     
 	PolarCV() {
         config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
         configSwitch(SPEED_MULT_PARAM, 0.f, 1.f, 0.f, "");
         configSwitch(PREV_EQ_PARAM, 0.f, 1.f, 0.f, "");
         configSwitch(NEXT_EQ_PARAM, 0.f, 1.f, 0.f, "");
-        configInput(SPEED_INPUT, "");
+        configInput(SPEED_INPUT, "CV for Speed");
         configParam(SPEED_DIAL_PARAM,  0.0001f, 0.02f, 0.01f, "Speed", " screen/PI", 0.f, 0.0001f);
         configParam(A_DIAL_PARAM, 0, 20, 1, "A", " V", 0.f, 0.5f);
         configParam(B_DIAL_PARAM, 0, 20, 1, "B", " V", 0.f, 0.5f);
+        configParam(CUR_EQ_PARAM, 0, 3, 1);
         getParamQuantity(A_DIAL_PARAM)->snapEnabled = true;
         getParamQuantity(B_DIAL_PARAM)->snapEnabled = true;
         configOutput(R_OUT_OUTPUT, "R");
@@ -90,18 +93,28 @@ struct PolarCV : Module {
             time_mult_idx = (time_mult_idx + 1) % 3;
         }
         prev_time_mult = double_time;
+        
+        speed_input_connected = inputs[SPEED_INPUT].isConnected();
+        if (speed_input_connected){
+            float speed_inp = inputs[SPEED_INPUT].getVoltage();
+            speed_inp = clamp(speed_inp, 0.f, 10.f);
+            speed = 0.0001f + speed_range_slope * (speed_inp - 0.f);
+        }
+        else{
+            speed = params[SPEED_DIAL_PARAM].getValue();
+        }
 
         bool next_trig = !params[NEXT_EQ_PARAM].getValue();
         if (next_trig != prev_next_eq_trig){
-            current_equation = (current_equation + 1) % max_eqs;
+            params[CUR_EQ_PARAM].setValue((int(params[CUR_EQ_PARAM].getValue() + 1)) % max_eqs);
         }
         prev_next_eq_trig = next_trig;
         
         bool prev_trig = !params[PREV_EQ_PARAM].getValue();
         if (prev_trig != prev_prev_eq_trig){
-            current_equation = current_equation - 1;
-            if (current_equation < 0){
-                current_equation = max_eqs - 1;
+            params[CUR_EQ_PARAM].setValue(params[CUR_EQ_PARAM].getValue() - 1);
+            if (params[CUR_EQ_PARAM].getValue() < 0){
+                params[CUR_EQ_PARAM].setValue(max_eqs - 1);
             }
         }
         prev_prev_eq_trig = prev_trig;
@@ -113,13 +126,20 @@ struct PolarCV : Module {
         outputs[X_OUT_OUTPUT].setVoltage(carts.x*10.0f);
         outputs[Y_OUT_OUTPUT].setVoltage(carts.y*10.0f);
         
+        lights[SPEED_MULT_LIGHT + 0].setBrightness(color_lights[time_mult_idx][0]);
+        lights[SPEED_MULT_LIGHT + 1].setBrightness(color_lights[time_mult_idx][1]);
+        lights[SPEED_MULT_LIGHT + 2].setBrightness(color_lights[time_mult_idx][2]);
+        
+        //lights[NEXT_EQ_LIGHT].setBrightness(params[NEXT_EQ_PARAM].getValue());
+        lights[PREV_EQ_LIGHT].setBrightness(0.5f);
+        lights[NEXT_EQ_LIGHT].setBrightness(0.5f);
+        
         updateTheta();
-        INFO("in process function: %f %f", current_cursor.x,current_cursor.y);
 	}
     
     void updateTheta() {
         
-        theta_delta = params[SPEED_DIAL_PARAM].getValue() * M_PI / 1000.0f * (time_mults[time_mult_idx] * time_mults[time_mult_idx]) + 0.1f;//0.000001f;
+        theta_delta = speed * M_PI / 1000.0f * (time_mults[time_mult_idx] * time_mults[time_mult_idx]) + 0.000001f;
         
         cur_theta = cur_theta + theta_delta;
         
@@ -127,7 +147,7 @@ struct PolarCV : Module {
         float B = params[B_DIAL_PARAM].getValue() / 2.0f;
         
         float r;
-        switch (current_equation) {
+        switch (int(params[CUR_EQ_PARAM].getValue())) {
             case 0:
                 r = sin(A * cur_theta) * cos(B * cur_theta);
                 break;
@@ -227,14 +247,15 @@ struct PolarCVDisplay : LedDisplay {
         nvgResetScissor(args.vg);
         nvgRestore(args.vg);
         
-        
-        INFO("in drawCursor function: %f %f", current_cursor.x,current_cursor.y);
     }
     
-    void drawStats(const DrawArgs& args, Vec pos, const char* title, const Stats& stats, float A, float B, int current_equation, float time_mult, float cur_theta) {
+    void drawStats(const DrawArgs& args, Vec pos, const char* title, const Stats& stats, float A, float B, float current_equation_t, float time_mult, float cur_theta) {
         std::shared_ptr<Font> font = APP->window->loadFont(fontPath);
         if (!font)
             return;
+        
+        int current_equation = int(current_equation_t);
+        
         nvgFontSize(args.vg, 13);
         nvgFontFaceId(args.vg, font->handle);
         nvgTextLetterSpacing(args.vg, -2);
@@ -246,27 +267,25 @@ struct PolarCVDisplay : LedDisplay {
         pos = pos.plus(Vec(22, 11));
 
         std::string text;
-        text = u8"\u0394";
-        text += std::to_string(time_mult).substr(0, 3);
-        //float pp = stats.max - stats.min;
-        //text += isNear(pp, 0.f, 100.f) ? string::f("% 6.2f", pp) : "  ---";
+        text = "";
+        text += std::to_string(time_mult).substr(0, 3) + "x";
         nvgText(args.vg, pos.x, pos.y, text.c_str(), NULL);
+        
+        text = "f = ";
         switch (current_equation) {
             case 0:
-                text = std::to_string((floor((A*2)+0.5)/2)).substr(0, 3) + "sin(thetaθ) + " + std::to_string((floor((B*2)+0.5)/2)).substr(0, 3) + "cos(theta)";
+                text += std::to_string((floor((A*2)+0.5)/2)).substr(0, 3) + "sin(" + u8"\u00D8" + ") + " + std::to_string((floor((B*2)+0.5)/2)).substr(0, 3) + "cos(" + u8"\u00D8" + ")";
                 break;
             case 1:
-                text = "sin("+std::to_string((floor((A*2)+0.5)/2)).substr(0, 3)+"cos(" + std::to_string((floor((B*2)+0.5)/2)).substr(0, 3) + "theta))";
+                text += "sin("+std::to_string((floor((A*2)+0.5)/2)).substr(0, 3)+"cos(" + std::to_string((floor((B*2)+0.5)/2)).substr(0, 3)  +" " + u8"\u00D8" + "))";
                 break;
             case 2:
-                text = "cos("+std::to_string((floor((A*2)+0.5)/2)).substr(0, 3)+"cos(" + std::to_string((floor((B*2)+0.5)/2)).substr(0, 3) + "theta))";
+                text += "cos("+std::to_string((floor((A*2)+0.5)/2)).substr(0, 3)+"cos(" + std::to_string((floor((B*2)+0.5)/2)).substr(0, 3) + " " + u8"\u00D8" + "))";
                 break;
             case 3:
-                text = "sin("+std::to_string((floor((A*2)+0.5)/2)).substr(0, 3)+"/"+std::to_string((floor((B*2)+0.5)/2)).substr(0, 3)+"theta)";
+                text += "sin("+std::to_string((floor((A*2)+0.5)/2)).substr(0, 3)+"/"+std::to_string((floor((B*2)+0.5)/2)).substr(0, 3)+ " " + u8"\u00D8" + ")";
                 break;
         }
-        
-        text += std::to_string(cur_theta);
         
         nvgText(args.vg, pos.x + 58 * 1, pos.y, text.c_str(), NULL);
     }
@@ -289,7 +308,9 @@ struct PolarCVDisplay : LedDisplay {
         }
     }
     
-    void updatePoints(float A, float B, int current_equation){
+    void updatePoints(float A, float B, float current_equation_t){
+        
+        int current_equation = int(current_equation_t);
         
         float theta = 0.00f;
         float r;
@@ -309,12 +330,10 @@ struct PolarCVDisplay : LedDisplay {
                 break;
             case 3:
                 r = sin((A/B) *theta);
-                min_repeat_pi  = 8.0f * M_PI;
+                min_repeat_pi  = 16.0f * M_PI;
                 break;
                 
         }
-        //float min_repeat_pi = findLCM((2.0f/A + 2.0f/B)*M_PI, 2.0f*M_PI);
-        
         
         for (int i = 0; i < BUFFER_SIZE; i++){
             Vec tempp;
@@ -352,9 +371,6 @@ struct PolarCVDisplay : LedDisplay {
         
         currentLayer += 1;
 
-        // Background lines
-        //drawBackground(args);
-
         if (!module)
             return;
 
@@ -363,16 +379,14 @@ struct PolarCVDisplay : LedDisplay {
         
         
         if (currentLayer % 3 == 0){
-            updatePoints(A, B, module->current_equation);
+            updatePoints(A, B, module->params[PolarCV::CUR_EQ_PARAM].getValue());
         }
         
         drawEq(args, A, B, module->cur_theta, current_points);
         
-        INFO("in drawLayer function: %f %f", module->current_cursor.x,module->current_cursor.y);
-        
         drawCursor(args, A, B, module->cur_theta, module->current_cursor);
         
-        drawStats(args, Vec(0, 0 + 1), "1", statsX, A, B, module->current_equation, module->time_mults[module->time_mult_idx], module->cur_theta);
+        drawStats(args, Vec(0, 0 + 1), "1", statsX, A, B, module->params[PolarCV::CUR_EQ_PARAM].getValue(), module->time_mults[module->time_mult_idx], module->cur_theta);
     }
     
 };
@@ -387,11 +401,7 @@ struct PolarCVWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		/*addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(8.467, 86.36)), module, PolarCV::SPEED_MULT_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(33.02, 86.36)), module, PolarCV::PREV_EQ_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(57.573, 86.36)), module, PolarCV::NEXT_EQ_PARAM));
-        */
-        addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(8.467, 86.36)), module, PolarCV::SPEED_MULT_PARAM, PolarCV::SPEED_MULT_LIGHT));
+        addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<RedGreenBlueLight>>>(mm2px(Vec(8.467, 86.36)), module, PolarCV::SPEED_MULT_PARAM, PolarCV::SPEED_MULT_LIGHT));
         addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(33.02, 86.36)), module, PolarCV::PREV_EQ_PARAM, PolarCV::PREV_EQ_LIGHT));
         addParam(createLightParamCentered<VCVLightLatch<MediumSimpleLight<WhiteLight>>>(mm2px(Vec(57.573, 86.36)), module, PolarCV::NEXT_EQ_PARAM, PolarCV::NEXT_EQ_LIGHT));
         
@@ -405,10 +415,8 @@ struct PolarCVWidget : ModuleWidget {
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(33.02, 115.147)), module, PolarCV::X_OUT_OUTPUT));
 		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(45.212, 115.147)), module, PolarCV::Y_OUT_OUTPUT));
 
-		// mm2px(Vec(66.04, 66.04))
-		//addChild(createWidget<Widget>(mm2px(Vec(0.0, 13.039))));
         PolarCVDisplay* display = createWidget<PolarCVDisplay>(mm2px(Vec(0.0, 13.039)));
-        display->box.size = mm2px(Vec(66.04, 66.04));//mm2px(Vec(66.04, 55.88));
+        display->box.size = mm2px(Vec(66.04, 66.04));
         display->module = module;
         display->moduleWidget = this;
         addChild(display);
